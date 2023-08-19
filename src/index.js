5c8b106802c375b22ca612973408fdf38ca51f6a
Kai Larson <kairblarson@gmail.com>
2023-08-19 16:24:29 -0400

    bugs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+    <Router>
         <ParallaxProvider>
             <App />
         </ParallaxProvider>
-    </React.StrictMode>
+    </Router>
 );
