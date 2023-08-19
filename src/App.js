5d10f9a8afcd9b7e193b9f5d2fb123c914a49d5d
Kai Larson <kairblarson@gmail.com>
2023-08-19 16:34:33 -0400

    okay now it should work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -7,13 +7,11 @@ import Navbar from "./components/Navbar";
 function App() {
     return (
         <div className="container">
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/projects" element={<Projects />} />
-                </Routes>
-            </Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,3 @@ const Footer = () => {
 };
 
 export default Footer;
-//create footer
-//add burger menu
-//have the random buttons do something
-//add form validation on the email form
-//add success/failure prompts when email is send
-//add a loading icon on the submit button when the promise is loading
-//add a profile pic or something and my name on the navbar
